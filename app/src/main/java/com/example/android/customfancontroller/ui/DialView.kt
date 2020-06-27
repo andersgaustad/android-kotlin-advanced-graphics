9f68eb947fc2efcf3ec2f2ffd79fc807e9dcb602
andersgaustad <anders.gaustad@gmail.com>
2020-06-27 21:06:36 +0200

    Added more accessibility support

--- app/src/main/java/com/example/android/customfancontroller/ui/DialView.kt
+++ app/src/main/java/com/example/android/customfancontroller/ui/DialView.kt
@@ -4,7 +4,11 @@ import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
+import android.view.accessibility.AccessibilityNodeInfo
 import androidx.core.content.withStyledAttributes
+import androidx.core.view.AccessibilityDelegateCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.accessibility.AccessibilityNodeInfoCompat
 import com.example.android.customfancontroller.R
 import kotlin.math.cos
 import kotlin.math.min
@@ -62,13 +66,33 @@ class DialView @JvmOverloads constructor(
             fanSpeedMaxColor = getColor(R.styleable.DialView_fanColor3, 0)
         }
 
+        ViewCompat.setAccessibilityDelegate(this, object : AccessibilityDelegateCompat() {
+            override fun onInitializeAccessibilityNodeInfo(host: View,
+                                                           info: AccessibilityNodeInfoCompat
+            ) {
+                super.onInitializeAccessibilityNodeInfo(host, info)
+
+                val customClick = AccessibilityNodeInfoCompat.AccessibilityActionCompat(
+                    AccessibilityNodeInfo.ACTION_CLICK,
+                    context.getString(
+                        if (fanSpeed !=  FanSpeed.HIGH) R.string.change else R.string.reset
+                    )
+                )
+
+                info.addAction(customClick)
+            }
+
+        })
+
+        updateContentDescription()
+
     }
 
     override fun performClick(): Boolean {
         if (super.performClick()) return true
 
         fanSpeed = FanSpeed.next(fanSpeed)
-        contentDescription = resources.getString(fanSpeed.label)
+        updateContentDescription()
 
         invalidate()
 
@@ -114,6 +138,10 @@ class DialView @JvmOverloads constructor(
 
 
 
+    }
+
+    fun updateContentDescription() {
+        contentDescription = resources.getString(fanSpeed.label)
     }
 
     // Helpers
